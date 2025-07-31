f4f97bf2cca330636c14c6230d050a4d88fb03b8
jw-02-pfda <johnywakim02@gmail.com>
2025-07-31 19:35:40 +0300

    started setting up the readme and added information on list append() timing

--- code/lists/insertion.py
+++ code/lists/insertion.py
@@ -1,15 +1,23 @@
 from code.utils.decorators.time_decorator import timeit
 
+N_ITER = 1_000_000
 
-@timeit
-def insert_element(list: list, element: object):
+
+@timeit(n_iter=N_ITER)
+def append_element(list: list, element: object):
     list.append(element)
 
 
-def test_insert_speed():
+def test_append_speed_3el():
     dog_breeds = ["chihuahua", "bulldog", "butterfly"]
-    insert_element(dog_breeds, "bernese")
+    append_element(dog_breeds, "bernese")
+
+
+def test_append_speed_1000el():
+    my_list = [i for i in range(1001)]
+    append_element(my_list, 1001)
 
 
 if __name__ == "__main__":
-    test_insert_speed()
+    test_append_speed_3el()
+    test_append_speed_1000el()
--- code/utils/decorators/time_decorator.py
+++ code/utils/decorators/time_decorator.py
@@ -1,18 +1,18 @@
 import time
 from functools import wraps
 
-N_ITER = 1000
 
+def timeit(n_iter: int):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start = time.perf_counter()
+            for i in range(n_iter):
+                result = func(*args, **kwargs)
+            end = time.perf_counter()
+            duration = (end - start) / n_iter
+            print(f"{func.__name__} took {duration:.8f} seconds")
 
-def timeit(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time.perf_counter()
-        for i in range(N_ITER):
-            result = func(*args, **kwargs)
-        end = time.perf_counter()
-        duration = (end - start) / N_ITER
-        print(f"{func.__name__} took {duration:.8f} seconds")
-        return result
+        return wrapper
 
-    return wrapper
+    return decorator

0245bc9f180fd7eb1914419a0643eec272350d7d
jw-02-pfda <johnywakim02@gmail.com>
2025-08-03 13:30:18 +0300

    adding results for the creation of lists of random floats

--- code/lists/creation.py
+++ code/lists/creation.py
@@ -1,20 +1,28 @@
 from code.utils.decorators.time_decorator import timeit
 import matplotlib.pyplot as plt
+import random
+
+N_ITER = 100
 
 
-@timeit(n_iter=100)
+@timeit(n_iter=N_ITER)
 def create_list(n_el: int):
     return [0] * n_el
 
 
-@timeit(n_iter=100)
+@timeit(n_iter=N_ITER)
 def create_list_from_iterable(n_el: int):
     return list(range(n_el))
 
 
-@timeit(n_iter=100)
+@timeit(n_iter=N_ITER)
 def create_list_comprehension(n_el: int):
     return [i for i in range(n_el)]
+
+
+@timeit(n_iter=N_ITER)
+def create_random_float_list(n_el: int):
+    return [random.uniform(0, 100) for _ in range(n_el)]
 
 
 def plot_graph(x_values, y_values):
@@ -38,15 +46,19 @@ if __name__ == "__main__":
         80_000_000,
         100_000_000,
     ]
-    print("> Creating lists")
+    # print("> Creating lists")
+    # print("-" * 58)
+    # times = [create_list(size)[0] for size in list_sizes]
+    # plot_graph(list_sizes, times)
+    # print("> Creating lists from iterables")
+    # print("-" * 58)
+    # times = [create_list_from_iterable(size)[0] for size in list_sizes]
+    # plot_graph(list_sizes, times)
+    # print("> Creating lists using list comprehensions")
+    # print("-" * 58)
+    # times = [create_list_comprehension(size)[0] for size in list_sizes]
+    # plot_graph(list_sizes, times)
+    print("> Creating lists of random floats between 0 and 100")
     print("-" * 58)
-    times = [create_list(size)[0] for size in list_sizes]
-    plot_graph(list_sizes, times)
-    print("> Creating lists from iterables")
-    print("-" * 58)
-    times = [create_list_from_iterable(size)[0] for size in list_sizes]
-    plot_graph(list_sizes, times)
-    print("> Creating lists using list comprehensions")
-    print("-" * 58)
-    times = [create_list_comprehension(size)[0] for size in list_sizes]
+    times = [create_random_float_list(size)[0] for size in list_sizes]
     plot_graph(list_sizes, times)

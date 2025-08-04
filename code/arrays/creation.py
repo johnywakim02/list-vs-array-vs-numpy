6244bb008dc955b2f652e2c7c517ef4dda2ec54b
jw-02-pfda <johnywakim02@gmail.com>
2025-08-04 12:02:36 +0300

    added result of comparison between array-like structures for creating one with 100M floats

---
+++ code/arrays/creation.py
@@ -0,0 +1,14 @@
+import random
+import array
+from code.utils.decorators.time_decorator import timeit
+
+N_ITER = 100
+
+
+@timeit(n_iter=N_ITER)
+def create_random_float_array(n_el: int):
+    return array.array("f", [random.uniform(0, 100) for _ in range(n_el)])
+
+
+if __name__ == "__main__":
+    create_random_float_array(100_000_000)

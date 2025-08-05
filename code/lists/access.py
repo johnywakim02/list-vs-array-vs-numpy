2bf0829413184d035762b3f0b3b91ee49bf1de1b
jw-02-pfda <johnywakim02@gmail.com>
2025-08-05 21:14:45 +0300

    added code and results for accessing and writing in a list

--- code/lists/access.py
+++ code/lists/access.py
@@ -10,8 +10,16 @@ def read_element(lst: list, pos: int):
     return lst[pos]
 
 
+@timeit(n_iter=N_ITER)
+def write_element(lst: list, pos: int, val: object):
+    lst[pos] = val
+
+
 if __name__ == "__main__":
     lst = [random.uniform(0, 100) for _ in range(100_000_000)]
     read_element(lst, 100)
     read_element(lst, 100_000)
     read_element(lst, 99_999_998)
+    write_element(lst, 100, 5.02)
+    write_element(lst, 100_000, 72.8)
+    write_element(lst, 99_999_998, "hmmm")

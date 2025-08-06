7fc41996df5cda8e41855c37bc0266f62b3fb91e
jw-02-pfda <johnywakim02@gmail.com>
2025-08-06 04:37:58 +0300

    added results for accessing numpy arrays

---
+++ code/numpy/access.py
@@ -0,0 +1,24 @@
+import numpy as np
+from code.utils.decorators.time_decorator import timeit
+
+N_ITER = 100_000_000
+
+
+@timeit(n_iter=N_ITER)
+def read_element(arr: np.ndarray, pos: int):
+    return arr[pos]
+
+
+@timeit(n_iter=N_ITER)
+def write_element(arr: np.ndarray, pos: int, val: object):
+    arr[pos] = val
+
+
+if __name__ == "__main__":
+    arr = np.random.rand(100_000_000) * 100
+    read_element(arr, 100)
+    read_element(arr, 100_000)
+    read_element(arr, 99_999_998)
+    write_element(arr, 100, 5.02)
+    write_element(arr, 100_000, 72.8)
+    write_element(arr, 99_999_998, 99)

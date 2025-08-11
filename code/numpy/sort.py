b4c3f8e4f763706979aa6f00a3d2712dea195bce
jw-02-pfda <johnywakim02@gmail.com>
2025-08-11 21:24:54 +0300

    coded and added the sorting performance for numpy arrays

---
+++ code/numpy/sort.py
@@ -0,0 +1,16 @@
+import numpy as np
+from code.utils.decorators.time_decorator import timeit
+from config import config
+
+N_ITER = config.N_ITER
+
+
+@timeit(n_iter=N_ITER)
+def sort_numpy_array(arr: np.ndarray) -> np.ndarray:
+    return np.sort(arr)
+
+
+if __name__ == "__main__":
+    n_el = 10_000_000
+    arr = np.random.uniform(0, 100, n_el)
+    sort_numpy_array(arr)

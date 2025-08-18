89512f7a64277736bdec59a37cd8f0f58cb47385
jw-02-pfda <johnywakim02@gmail.com>
2025-08-18 19:29:41 +0300

    added results for searching for elements in a list

---
+++ code/lists/search.py
@@ -0,0 +1,50 @@
+from code.utils.decorators.time_decorator import timeit
+from config import config
+
+N_ITER = config.N_ITER
+
+
+@timeit(n_iter=N_ITER)
+def search_element_presence(lst: list[int], el: int):
+    return el in lst
+
+
+@timeit(n_iter=N_ITER)
+def search_element_first_position(lst: list[int], el: int):
+    try:
+        lst.index(el)
+    except ValueError:
+        print(f"Element {el} was not found in the list")
+
+
+@timeit(n_iter=N_ITER)
+def search_element_all_positions(lst: list[int], el: int):
+    return [i for i, x in enumerate(lst) if x == el]
+
+
+@timeit(n_iter=N_ITER)
+def search_element_all_positions_v2(lst: list[int], el: int):
+    indices = []
+    start = 0
+    while True:
+        try:
+            idx = lst.index(el, start)
+            indices.append(idx)
+            start = idx + 1
+        except ValueError:
+            break
+    return indices
+
+
+if __name__ == "__main__":
+    lst = [i + 1 for i in range(100_000_000)]
+    elements = [100, 10_000, 1_000_000, 100_000_000]
+    methods = [
+        search_element_presence,
+        search_element_first_position,
+        search_element_all_positions,
+        search_element_all_positions_v2,
+    ]
+    for method in methods:
+        for el in elements:
+            method(lst, el)

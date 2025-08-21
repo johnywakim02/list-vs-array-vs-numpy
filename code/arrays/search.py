c9a7c3d83d84e06ffeda29e1bdefacdd0a72bc54
jw-02-pfda <johnywakim02@gmail.com>
2025-08-21 21:34:29 +0300

    added results for searching arrays of the array module

---
+++ code/arrays/search.py
@@ -0,0 +1,36 @@
+import array
+from code.utils.decorators.time_decorator import timeit
+from config import config
+
+N_ITER = config.N_ITER
+
+
+@timeit(n_iter=N_ITER)
+def arr_search_element_presence(arr: array.array, el: int):
+    return el in arr
+
+
+@timeit(n_iter=N_ITER)
+def arr_search_element_first_position(arr: array.array, el: int):
+    try:
+        return arr.index(el)
+    except ValueError:
+        print(f"Element {el} was not found in the array.")
+
+
+@timeit(n_iter=N_ITER)
+def arr_search_element_all_positions(arr: array.array, el: int):
+    return [i for i, x in enumerate(arr) if x == el]
+
+
+if __name__ == "__main__":
+    arr = array.array("I", range(1, 100_000_001))  # 'I' = unsigned int
+    elements = [100, 10_000, 1_000_000, 100_000_000]
+    methods = [
+        arr_search_element_presence,
+        arr_search_element_first_position,
+        arr_search_element_all_positions,
+    ]
+    for method in methods:
+        for el in elements:
+            method(arr, el)

58f7b9d65ba6376d03cf557d699bf0f11bd7b73a
jw-02-pfda <johnywakim02@gmail.com>
2025-09-10 22:10:38 +0300

    created slicing code for arrays from array module

---
+++ code/arrays/slice.py
@@ -0,0 +1,45 @@
+from array import array
+from code.utils.decorators.time_decorator import timeit
+from code.utils.methods.draw import plot_graph
+from code.utils.models.slice_range import SliceRange
+from config import config
+
+N_ITER = config.N_ITER
+
+
+@timeit(n_iter=N_ITER)
+def get_element_range(arr: array, range: SliceRange):
+    return arr[range.start : range.end]
+
+
+if __name__ == "__main__":
+    n_el: int = 100_000_000
+    arr: array = array("i", [0] * n_el)  # "i" = signed int
+    factors: list[float] = [0.000001, 0.0001, 0.01, 0.1, 0.2, 0.4, 0.8, 1.0]
+
+    # slicing's relation with number of elements sliced
+    ranges: list[SliceRange] = []
+    pos = 0
+    for factor in factors:
+        ranges.append(SliceRange(pos, pos + int(factor * n_el)))
+    times = [get_element_range(arr, slice_range)[0] for slice_range in ranges]
+
+    plot_title = "Slicing Time vs Slice Size in an array of 100M elements, slicing from the beginning"
+    plot_graph(
+        [factor * n_el for factor in factors],
+        times,
+        plot_title,
+        "Slicing Time (s)",
+        "Slice Size",
+    )
+
+    # slicing's relation with position
+    ranges = []
+    slice_size = 1_000_000
+    positions = range(0, n_el - slice_size, slice_size * 10)
+    for position in positions:
+        ranges.append(SliceRange(position, position + int(slice_size)))
+    times = [get_element_range(arr, slice_range)[0] for slice_range in ranges]
+
+    plot_title = "Slicing Time vs Slice Position in an array of 100M elements, for a slice Size of 1M elements"
+    plot_graph(positions, times, plot_title, "Slicing Time (s)", "Slice Position")
